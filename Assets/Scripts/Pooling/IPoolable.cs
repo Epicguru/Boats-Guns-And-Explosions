5905b93bc3b9570790d18d7510f64a1ce38bfa7c
James Billy <james@epicguru.co.uk>
2018-04-17 22:00:20 +0200

    Working on smooth waves...

---
+++ Assets/Scripts/Pooling/IPoolable.cs
@@ -0,0 +1,16 @@
+﻿
+public interface IPoolable
+{
+    /// <summary>
+    /// Called when borrowed from the pool of objects, may be before or after Unity methods such as Start or Awake.
+    /// Here you should active the game object, parent/unparent it etc.
+    /// </summary>
+    void Begin();
+
+    /// <summary>
+    /// Called when returning to the pool. This should remove any resources held (unless they can be reused) and prepare this object
+    /// to have Begin() called again.
+    /// Here you should deactiveate/hide the game object, unparent/parent it etc.
+    /// </summary>
+    void End();
+}
---
+++ Assets/Scripts/Pooling/ObjectPool.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public abstract class ObjectPool<T> : MonoBehaviour where T: IPoolable
+{
+    public Queue<T> Pooled = new Queue<T>();
+
+    public virtual int CurrentlyPooled
+    {
+        get
+        {
+            if (Pooled == null)
+                return 0;
+            return Pooled.Count;
+        }
+    }
+    public int OnLease { get; private set; }
+    public int Total { get; private set; }
+
+    public abstract T CreateNew();
+
+    public virtual T GetFromPool()
+    {
+        if(CurrentlyPooled > 0)
+        {
+            T fromPool = Pooled.Dequeue();
+            OnLease++;
+
+            fromPool.Begin();
+
+            return fromPool;
+        }
+        else
+        {
+            // None are pooled, create new
+            T newObj = CreateNew();
+            if(newObj == null)
+            {
+                Debug.LogError("Why did an object pool return null from the CreateNew method?");
+                return default(T);
+            }
+            Total++;
+            OnLease++;
+
+            newObj.Begin();
+
+            return newObj;
+        }
+    }
+
+    public virtual void ReturnToPool(T obj)
+    {
+        if(obj == null)
+        {
+            return;
+        }
+
+        OnLease--;
+        obj.End();
+
+        Pooled.Enqueue(obj);
+    }
+}
