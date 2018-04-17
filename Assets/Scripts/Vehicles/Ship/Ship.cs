6d2c7ab3989f51c53084c7bb81ea38704377fc45
James Billy <james@epicguru.co.uk>
2018-04-17 17:46:32 +0200

    Adding basic code.

---
+++ Assets/Scripts/Vehicles/Ship/Ship.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class Ship : Vehicle
+{
+    [Range(0f, 1f)]
+    public float GeneralSize = 0.5f;
+
+    public override void ApplyPhysicsSettings()
+    {
+        base.ApplyPhysicsSettings();
+
+        // Linear and angular drag
+        Rigidbody.angularDrag = GetAngularDrag();
+        Rigidbody.drag = GetLinearDrag();
+
+        // Collision detection and interpolation.
+        Rigidbody.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        Rigidbody.interpolation = RigidbodyInterpolation2D.Interpolate;
+    }
+
+    public virtual float GetLinearDrag()
+    {
+        // Very small ships have linear drag 3 and largest ships have 10.
+        return Mathf.Lerp(3f, 10f, GeneralSize);
+    }
+
+    public virtual float GetAngularDrag()
+    {
+        // Very small ships have angular drag 5 and largest ships have 10.
+        return Mathf.Lerp(5f, 10f, GeneralSize);
+    }
+}
---
+++ Assets/Scripts/Vehicles/Vehicle.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public abstract class Vehicle : NetworkBehaviour
+{
+    // All vehicles are physics based moving objects.
+
+    public Rigidbody2D Rigidbody
+    {
+        get
+        {
+            if(_rigidbody == null)
+            {
+                _rigidbody = GetComponent<Rigidbody2D>();
+                if(_rigidbody == null)
+                {
+                    Debug.LogError("Tried to access the rigidbody on this vehicle, but it is missing!");
+                }
+            }
+            return _rigidbody;
+        }
+    }
+    private Rigidbody2D _rigidbody;
+
+    public float BaseMass = 1f;
+
+    public void Awake()
+    {
+        ApplyPhysicsSettings();
+    }
+
+    public virtual void ApplyPhysicsSettings()
+    {
+        Rigidbody.useAutoMass = false;
+        if(BaseMass <= 0)
+        {
+            Debug.LogError("This '{0}' has a mass of {1}, invalid mass! Must be greater than 0. Using mass of 0.1".Form(name, BaseMass));
+            Rigidbody.mass = 0.1f;
+        }
+        else
+        {
+            Rigidbody.mass = BaseMass;
+        }
+    }
+}
