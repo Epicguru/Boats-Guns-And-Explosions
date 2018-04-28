0e9e043da370dbe033da2aea54519ab82496e082
James Billy <james@epicguru.co.uk>
2018-04-28 20:56:35 +0200

    Improving UI and added basic overview funcitonality.

---
+++ Assets/Scripts/UI/Ship Overview/UI_ShipOverview.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_ShipOverview : MonoBehaviour
+{
+    public Ship Ship;
+
+    public UI_StateBar HealthBar;
+    public UI_StateBar HullWaterBar;
+    public UI_StateBar SunkBar;
+
+    public Text Title;
+
+    public CanvasGroup Group;
+    public AnimationCurve Curve;
+    public float OpenTime;
+
+    [ReadOnly]
+    public bool IsExtended;
+    public RectTransform ExtendedIcon;
+
+    private float timer;
+
+    public void OnExtendedButton()
+    {
+        IsExtended = !IsExtended;
+    }
+
+    public void Start()
+    {
+        timer = 0f;
+    }
+
+    public void Update()
+    {
+        // Update opening and closing.
+        UpdateOpenClose();
+    }
+
+    private void UpdateOpenClose()
+    {
+        if (IsOpen())
+        {
+            timer += Time.unscaledDeltaTime;
+            if (timer > OpenTime)
+                timer = OpenTime;
+        }
+        else
+        {
+            timer -= Time.unscaledDeltaTime;
+            if (timer < 0f)
+                timer = 0f;
+        }
+
+        float p = Mathf.Clamp01(timer / OpenTime);
+        Group.alpha = Curve.Evaluate(p);
+
+        if (Ship != null)
+            Title.text = Ship.Unit.Name;
+
+        if (Ship != null)
+        {
+            UpdateVisuals();
+        }
+    }
+
+    public bool IsOpen()
+    {
+        return Ship != null;
+    }
+
+    public void UpdateVisuals()
+    {
+        if (Ship == null)
+            return;
+
+        HealthBar.Percentage = Ship.DamageModel.GetAverageHealthPercentage();
+        HullWaterBar.Percentage = Ship.Damage.GetWaterPercentage();
+        SunkBar.Percentage = Ship.Damage.GetSinkState();
+    }
+}
---
+++ Assets/Scripts/UI/Ship Overview/UI_StateBar.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_StateBar : MonoBehaviour
+{
+    public float Percentage
+    {
+        get
+        {
+            return _percentage;
+        }
+        set
+        {
+            if(value != _percentage)
+            {
+                _percentage = value;
+                UpdateBar();
+            }
+        }
+    }
+    [Header("Controls")]
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float _percentage = 1f;
+
+
+    public string Title
+    {
+        get
+        {
+            return _title;
+        }
+        set
+        {
+            if(value != _title)
+            {
+                _title = value;
+                UpdateTitle();
+            }
+        }
+    }
+    [SerializeField]
+    private string _title = "XYZ";
+
+    public Gradient Colours = new Gradient();
+
+    [Header("References")]
+    public Text TitleText;
+    public Image Bar;
+
+    public void Start()
+    {
+        UpdateBar();
+        UpdateTitle();
+    }
+
+    public void UpdateBar()
+    {
+        if (Bar == null)
+            return;
+
+        Bar.fillAmount = Mathf.Clamp01(Percentage);
+        Bar.color = Colours.Evaluate(Mathf.Clamp01(Percentage));
+    }
+
+    public void UpdateTitle()
+    {
+        if (TitleText == null)
+            return;
+
+        TitleText.text = Title;
+    }
+}
