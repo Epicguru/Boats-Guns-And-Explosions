61e5579ec92b3c0af563a38ce636027d551faad6
James Billy <james@epicguru.co.uk>
2018-05-01 16:50:39 +0200

    Added new loading scene, to load assets in a visually appealing way.

---
+++ Assets/Scripts/UI/Loading/UI_LoadingMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_LoadingMenu : MonoBehaviour
+{
+    [Header("Controls")]
+    public int TotalSteps;
+    public int CurrentStep;
+
+    public string SubLabel = "Loading xyz...";
+    [Range(0f, 1f)]
+    public float SubProgress;
+
+    [Header("References")]
+    public Text StepText;
+    public Text SubText;
+
+    public Image StepBar;
+    public Image SubBar;
+
+    public void Update()
+    {
+        StepText.text = CurrentStep + "/" + TotalSteps;
+        float p = (float)CurrentStep / TotalSteps;
+        StepBar.fillAmount = p;
+
+        SubText.text = SubLabel;
+        SubBar.fillAmount = SubProgress;
+    }
+}
