6101afe89f6b3ee2394d04f5bed60511c17247fe
James Billy <james@epicguru.co.uk>
2018-05-03 22:26:33 +0200

    Added new loading system.

--- Assets/Scripts/UI/Loading/UI_LoadingMenu.cs
+++ Assets/Scripts/UI/Loading/UI_LoadingMenu.cs
@@ -3,17 +3,33 @@ using UnityEngine.UI;
 
 public class UI_LoadingMenu : MonoBehaviour
 {
-    [Header("Controls")]
-    [Range(0f, 1f)]
-    public float Percentage;
+    public float Percentage
+    {
+        get
+        {
+            return StepBar.fillAmount;
+        }
+        set
+        {
+            StepBar.fillAmount = value;
+            StepText.text = (Percentage * 100f).ToString("n1") + "%";
+        }
+    }
+
+    public string Title
+    {
+        get
+        {
+            return TitleText.text;
+        }
+        set
+        {
+            TitleText.text = value;
+        }
+    }
 
     [Header("References")]
     public Text StepText;
+    public Text TitleText;
     public Image StepBar;
-
-    public void Update()
-    {
-        StepText.text = (Percentage * 100f).ToString("n1") + "%";
-        StepBar.fillAmount = Percentage;
-    }
 }
